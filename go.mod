9be3fafd82ad6f5e27281609f443b822095f6224
AA12GQ <875151567@qq.com>
2022-05-13 11:32:42 +0800

    集成 gin

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GDColumn
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.7 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
