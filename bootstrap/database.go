8c9bbc25e59bf2f5d5f3eca7ab47694b271d9982
AA12GQ <875151567@qq.com>
2022-05-14 09:51:00 +0800

    数据库请求日志

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -4,10 +4,10 @@ import (
 	"GDColumn/pkg/config"
 	"GDColumn/pkg/database"
 	"GDColumn/app/models/user"
+	"GDColumn/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"time"
 	"fmt"
 	"errors"
@@ -38,7 +38,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
