515bfd667e8ba2eb792a2cbcd4858d1287d47358
AA12GQ <875151567@qq.com>
2022-05-15 14:18:29 +0800

    play 命令

---
+++ app/cmd/play.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var CmdPlay = &cobra.Command{
+	Use:   "play",
+	Short: "Likes the Go Playground, but running at our application context",
+	Run:   runPlay,
+}
+
+func runPlay(cmd *cobra.Command, args []string) {
+
+}
--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	rootCmd.AddCommand(
 		cmd.CmdServe,
 		cmd.CmdKey,
+		cmd.CmdPlay,
 	)
 
 	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
