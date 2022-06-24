5a10bf5bd381b3719e29daa4186849e8fa0e2ece
AA12GQ <875151567@qq.com>
2022-06-24 13:02:00 +0800

    cache 包

---
+++ bootstrap/cache.go
@@ -0,0 +1,21 @@
+package bootstrap
+
+import (
+	"GDColumn/pkg/cache"
+	"GDColumn/pkg/config"
+	"fmt"
+)
+
+// SetupCache 缓存
+func SetupCache() {
+
+	// 初始化缓存专用的 redis client, 使用专属缓存 DB
+	rds := cache.NewRedisStore(
+		fmt.Sprintf("%v:%v", config.GetString("redis.host"), config.GetString("redis.port")),
+		config.GetString("redis.username"),
+		config.GetString("redis.password"),
+		config.GetInt("redis.database_cache"),
+	)
+
+	cache.InitWithCacheStore(rds)
+}
